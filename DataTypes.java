c52aaf634565c3bf47b6e4855d3bcac898d7f322
VanshTomar <vansh.tomar.1303@gmail.com>
2024-08-03 11:35:45 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+package com.vansh;
+
+public class DataTypes {
+    /*
+
+        DataType -> Two type of datatype - 1.Primitive Datatype 2.Non-Primitive Datatype
+
+        * Primitive Datatype (Instric)
+        '-> int ,byte ,float ,long ,char ,double ,boolean ,short these are the main.
+        '->Because of these 8 primitive datatype java is not 100% Object Oriented Language.
+
+        *Non-Primitive Datatype (Derived)
+        '-> classes,Strings,Arrays,Interface,Objects,etc
+
+     */
+}
---
+++ Variables.java
@@ -0,0 +1,18 @@
+package com.vansh;
+
+public class Variables {
+
+    // Variable -> It is a container which stores the values in (String,integer,double,boolean,etc)
+    // '-> It value can be changed during execution.
+
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 3.14f;
+        double c = 3.14;
+        long d = 343948439;
+        boolean e = true; //true/false
+        byte f = 127;
+        String s = "James";
+        String t = new String("James");
+    }
+}
